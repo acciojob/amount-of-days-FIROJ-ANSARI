b5d9544bae0fdcb3d57f22f6dfaea1d6eb7b10b2
FIROJ-ANSARI <212002firojansari@gmail.com>
2025-03-18 04:53:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 	function daysofAYear(params) {
-		if(params % 4 === 0){
-		params % 100 == 0;
-		params % 400 == 0;
-		console.log(366)
+		if(params % 400 == 0){
+		console.log(366);
 			return 366;
 	}
-     else{
-		return 365;
-	 }
-	}
+		else if (params % 100 == 0) {
+			console.log(365);
+			return 365;
+		}
+		else if (params % 4 == 0) {
+			console.log(366);
+				return 366;
+		}
+		else{
+			console.log(365)
+		}
 console.log(daysOfAYear(2022))
 console.log(daysOfAYear(2024))
 console.log(daysOfAYear(1900))
