55c7aa27f97d28102726d2fc92991bef45137031
FIROJ-ANSARI <212002firojansari@gmail.com>
2025-03-18 04:39:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//your JS code here. If required.
+	function daysofAYear(params) {
+		if(params % 4 == 0){
+		params % 100 == 0;
+		params % 400 == 0;
+		console.log("Leap year")
+			return LeapYear
+	}
+     else{
+		return notleapyear 
+	 }
+	}
+console.log(daysOfAYear(2022))
+console.log(daysOfAYear(2024))
+console.log(daysOfAYear(1900))
+console.log(daysOfAYear(2000))
